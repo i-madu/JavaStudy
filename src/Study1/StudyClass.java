d6aa5fca3df287ac44cb3af64fef43dc4ee42908
imadutomoha <imadutomoha@localhost>
2022-05-01 22:24:32 +0900

    [add]java導入

---
+++ src/Study1/StudyClass.java
@@ -0,0 +1,21 @@
+package Study1;
+
+public class StudyClass {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Java!!");
+		System.out.println("こんにちは");
+		System.out.println("31 + 31 の計算をします！");
+		System.out.println(31+31);
+		System.out.println(22-22);
+		System.out.println(-5*2);
+		System.out.println(6*6*3.14);
+		System.out.println("答えは"+64);
+		System.out.println("数学でx,yという文字を数式に用いるように、Javaも似たようなことができる!");
+		int x;  //	変数を入れる準備
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+
+}
