2a4ffca4d03d07ee971a77963894124afd1e6a09
imadutomoha <imadutomoha@localhost>
2022-05-02 18:13:26 +0900

    [add]java基本の書き方/変数/データ型

--- src/Study1/StudyClass.java
+++ src/Study1/StudyClass.java
@@ -5,10 +5,6 @@ public class StudyClass {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello Java!!");
-		System.out.println("こんにちは");
-		System.out.println("31 + 31 の計算をします！");
-		System.out.println(31+31);
-		System.out.println(22-22);
 		System.out.println(-5*2);
 		System.out.println(6*6*3.14);
 		System.out.println("答えは"+64);
@@ -17,6 +13,27 @@ public class StudyClass {
 		x = 6;
 		System.out.println(x * x * 3.14);
 		System.out.println("javaの３ステップ。①ソースコードの作成。②コンパイル。③実行。");
+		System.out.println("javaのソースコードを記述したファイル（ソースファイル）を保存するときは「クラス名.java」にする");
+		System.out.println("javaプログラムの始め方");
+		System.out.println("①どのようなプログラムを作りたいか考える");
+		System.out.println("②プログラムの名前を決める（クラス名が決まる）");
+		System.out.println("「クラス名.java」という名前でファイルを作る");
+		System.out.println("ソースコードの外側部分（クラスブロック）を記述");
+		System.out.println("ソースコードの内側部分（メソッドブロック）に命令を書いていく");
+		System.out.println("データ型（整数）");
+		byte glasses; glasses = 2;
+		short age; age = 18;
+		int salary; salary = 152000;  //	主にintを使う
+		long worldPeople; worldPeople = 6900000000L;
+		System.out.println("データ型（小数）");
+		float weight; weight = 67.5F;
+		double height; height = 171.2;  //doubleの方がメモリを消費するがより厳密に計算できる。小数点を使う場合、基本はdouble型にする。
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		
+		
 	}
 
 }
