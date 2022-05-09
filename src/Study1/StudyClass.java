04dd172c28be4df0947ab2d4edb357e9ee50e6d2
i-madu <jinjinyouye@gmail.com>
2022-05-09 23:52:01 +0900

    [add]記号

--- src/Study1/StudyClass.java
+++ src/Study1/StudyClass.java
@@ -50,6 +50,8 @@ public class StudyClass {
 		long peple = 314159265853979L;
 		String word = "港の攻撃！　敵に15ポイントのダメージを与えた！";
 		System.out.println("私の好きな記号は(\")です");
+		System.out.println("私の好きな記号は(\")です");
+		
 	}
 
 }
